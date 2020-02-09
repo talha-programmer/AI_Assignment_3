71431a224e7e70bcf849b195970237138bd793a0
talha0503 <talhashabbir9@gmail.com>
2020-02-09 11:54:09 +0500

    first commit

---
+++ Assignment3/assignment5.py
@@ -0,0 +1,105 @@
+"""Wordcount exercise
+Python class
+
+The main() below is already defined and complete. It calls print_words()
+and print_top() functions which you write.
+
+1. For the --count flag, implement a print_words(filename) function that counts
+how often each word appears in the text and prints:
+word1 count1
+word2 count2
+...
+
+Print the above list in order sorted by word (python will sort punctuation to
+come before letters -- that's fine). Store all the words as lowercase,
+so 'The' and 'the' count as the same word.
+
+2. For the --topcount flag, implement a print_top(filename) which is similar
+to print_words() but which prints just the top 20 most common words sorted
+so the most common word is first, then the next most common, and so on.
+
+Use str.split() (no arguments) to split on all whitespace.
+
+Workflow: don't build the whole program at once. Get it to an intermediate
+milestone and print your data structure and sys.exit(0).
+When that's working, try for the next milestone.
+
+Optional: define a helper function to avoid code duplication inside
+print_words() and print_top().
+
+"""
+
+import sys
+
+
+# Define print_words(filename) and print_top(filename) functions.
+# You could write a helper utility function that reads a file
+# and builds and returns a word/count dict for it.
+# Then print_words() and print_top() can just call the utility function.
+
+def saveInDict(word, dict):
+  if not word[0].isalpha() and len(word) > 1:
+    saveInDict(word[0], dict)
+    saveInDict(word[1:], dict)
+    return 
+  if not word[-1].isalpha() and len(word) > 1:
+    saveInDict(word[-1], dict)
+    saveInDict(word[:-1], dict)
+    return 
+  if word in dict:
+    dict[word] += 1
+  else:
+    dict[word] = 1
+    
+
+def createDict(filename):
+  st = ''
+  with open(filename, mode='r') as f:
+     st = f.read()
+  st = st.lower()
+  lis = st.split()
+  wordDict = dict()
+  for word in lis:
+    saveInDict(word, wordDict)
+  return wordDict
+
+def print_words(filename):
+  wordDict = createDict(filename)
+  print('Total Occurance of each word in file: ')
+  count = 0
+  for key in sorted(wordDict.keys()):
+    print(key + ": " + str(wordDict[key]), end='      ')
+    count +=1
+    if count==7:
+      print()
+      count = 0
+
+def print_top(filename):
+  wordDict = createDict(filename)
+  count = 0
+  for key,value in sorted(wordDict.items(), key= lambda kv:(kv[1], kv[0]), reverse=True):
+    print(key + ": " + str(value))
+    count += 1
+    if count == 20:
+      break
+###
+
+# This basic command line argument parsing code is provided and
+# calls the print_words() and print_top() functions which you must define.
+def main():
+  if len(sys.argv) != 3:
+    print ('usage: ./wordcount.py {--count | --topcount} file')
+    sys.exit(1)
+
+  option = sys.argv[1]
+  filename = sys.argv[2]
+  if option == '--count':
+    print_words(filename)
+  elif option == '--topcount':
+    print_top(filename)
+  else:
+    print ('unknown option: ' + option)
+    sys.exit(1)
+
+if __name__ == '__main__':
+  main()
